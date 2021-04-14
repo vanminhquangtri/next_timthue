0df5661c5efb46bc708baecd651b15e155459132
Van Minh <vanminhquangtri@gmail.com>
2021-04-14 18:03:20 +0700

    ok

--- pages/demos/redux-wrapper/index.jsx
+++ pages/demos/redux-wrapper/index.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import { wrapper } from 'src/next-store';
 
-export const getStaticProps = wrapper.getStaticProps(({ store, preview }) => {
-    console.log('2. Page.getStaticProps uses the store to dispatch things');
-    store.dispatch({
-        type: 'TICK',
-        payload: 'was set in other page ' + preview,
-    });
-});
+const Page = ({ custom }) => (
+    <div>
+        <div>Prop from Redux</div>
+        <div>Prop from getInitialProps: {custom}</div>
+    </div>
+);
 
-const ReduxWrapper = () => {
-    return <div>ReduxWrapper</div>;
-};
+export const getStaticProps = wrapper.getStaticProps(
+    async ({ store, req, res, ...etc }) => {
+        console.log('store.getState() :>> ', store.getState().tick);
+        store.dispatch({ type: 'FOO', payload: 'Redux wrapper' }); // The component can read from the store's state when rendered
+        console.log('store.getState() :>> ', store.getState().tick);
+        return {
+            props: {
+                custom: store.getState().tick,
+            },
+        }; // You can pass some custom props to the component from here
+    }
+);
 
-export default ReduxWrapper;
+export default connect((state) => state)(Page);
--- src/next-store/index.js
+++ src/next-store/index.js
@@ -1,34 +1,14 @@
 import { createStore } from 'redux';
-import { createWrapper, HYDRATE } from 'next-redux-wrapper';
+import { createWrapper } from 'next-redux-wrapper';
 
 // create your reducer
 const reducer = (state = { tick: 'init' }, action) => {
     switch (action.type) {
-        case HYDRATE:
-            return {
-                ...state,
-                server: {
-                    ...state.server,
-                    ...action.payload.server,
-                },
-            };
-        case 'SERVER_ACTION':
-            return {
-                ...state,
-                server: {
-                    ...state.server,
-                    tick: action.payload,
-                },
-            };
-        case 'CLIENT_ACTION':
-            return {
-                ...state,
-                client: {
-                    ...state.client,
-                    tick: action.payload,
-                },
-            };
         case 'TICK':
+            return { ...state, tick: action.payload };
+        case 'TOE':
+            return { ...state, tick: action.payload };
+        case 'FOO':
             return { ...state, tick: action.payload };
         default:
             return state;
