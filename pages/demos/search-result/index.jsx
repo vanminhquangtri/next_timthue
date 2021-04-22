4c2d6d80e358d64376964d03c690ae16b30bf5a0
Van Minh <vanminhquangtri@gmail.com>
2021-04-22 18:02:59 +0700

    ok

--- pages/demos/search-result/index.jsx
+++ pages/demos/search-result/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import Log from '../../../src/api/log';
 import cookies from 'next-cookies';
+import cookie from 'cookie';
 import Link from 'next/link';
 import Slug from 'slug';
 import { useCookies } from 'react-cookie';
@@ -9,7 +10,6 @@ import { useRouter } from 'next/router';
 import Texts from 'src/constants/texts';
 
 const SearchResult = ({ logList, limit }) => {
-    console.log('limit :>> ', limit);
     const router = useRouter();
     const setCookieLogId = (log_id) => {
         if (log_id) {
@@ -45,6 +45,12 @@ const SearchResult = ({ logList, limit }) => {
     useEffect(() => {
         return () => {
             setCookie('page', 1, {
+                path: '/',
+                expires: new Date(
+                    new Date().getTime() + Texts.expired_time_cookie
+                ),
+            });
+            setCookie('current_log_list', [], {
                 path: '/',
                 expires: new Date(
                     new Date().getTime() + Texts.expired_time_cookie
@@ -89,26 +95,60 @@ const SearchResult = ({ logList, limit }) => {
 };
 
 export async function getServerSideProps(ctx) {
-    const allCookies = cookies(ctx);
+    // get data from url
     const { action, page, limit } = ctx.query;
-    if (allCookies) {
-        const data = {
-            params: {
-                action,
+    const data = {
+        params: {
+            action,
+            limit,
+            page: page,
+        },
+        body: {},
+    };
+    const allCookies = cookies(ctx);
+    const currentLogListCookie = allCookies?.current_log_list || [];
+    console.log('currentLogListCookie :>> ', currentLogListCookie);
+
+    const response = await Log.getLogList(data);
+
+    // add new list of logs to current cookie, prepare for next api request
+    if (response?.success && response?.data?.length) {
+        await response?.data?.forEach((log) => {
+            currentLogListCookie.push(log);
+        });
+        // const newFullCookieLogList =
+        //     currentLogListCookie?.length > 0
+        //         ? [...currentLogListCookie, ...response?.data]
+        //         : response?.data;
+        // console.log('newFullCookieLogList :>> ', newFullCookieLogList);
+        console.log('currentLogListCookie :>> ', currentLogListCookie);
+        ctx.res.setHeader(
+            'Set-Cookie',
+            cookie.serialize(
+                'current_log_list',
+                JSON.stringify(currentLogListCookie),
+                {
+                    maxAge: Texts.expired_time_cookie / 1000,
+                    path: '/',
+                }
+            )
+        );
+    }
+    if (response?.success) {
+        const fullLog =
+            currentLogListCookie?.length > 0
+                ? [...currentLogListCookie, ...response?.data]
+                : response?.data;
+        return {
+            props: {
+                logList: fullLog,
                 limit,
-                page: page,
             },
-            body: {},
         };
-        const response = await Log.getLogList(data);
-        if (response) {
-            return {
-                props: {
-                    logList: response.data,
-                    limit,
-                },
-            };
-        }
+    } else {
+        return {
+            notFound: true,
+        };
     }
 }
 
